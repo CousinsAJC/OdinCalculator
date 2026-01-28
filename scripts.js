5b99db08ce982cb11e21f81d09fe9033e6d3fb0e
Andrew Cousins <cousinsdrew@gmail.com>
2026-01-27 20:08:09 -0500

    calculator is fully functioning, with most glitches and bugs fixed.  need to address numbers overflowing screen size.

--- scripts.js
+++ scripts.js
@@ -1,8 +1,6 @@
 let firstNum = null;
-let secondNum = null;
 let operator = null;
-let operationRequest = null;
-let calculatedTotal = null;
+let input = null;
 
 let screen = document.querySelector(".screen");
 screen.textContent = "0";
@@ -13,45 +11,61 @@ const handleClick = (event) => {
     console.log(event.target.textContent);
 
     //If press Enter Button
-    if (event.target.textContent == "Enter" && operator != null) {
-        secondNum = operationRequest;
-        firstNum = Number(firstNum);
-        secondNum = Number(secondNum);
-        calculatedTotal = operate(firstNum, operator, secondNum);
-        operationRequest = null;
-        screen.textContent = calculatedTotal;
+    if (event.target.textContent == "Enter") {
+        if (operator == null || input == null) {
+            //do nothing
+        } else {
+            if (firstNum != null && input != null && operator != null) {
+                firstNum = operate(Number(firstNum), operator, Number(input));
+                input = null;
+                operator = null;
+                screen.textContent = firstNum;
+            } else {
+                screen.textContent = "There seems to be an error.";
+                input = null;
+                firstNum = null;
+                operator = null;
+            }
+        }
 
     //If press Clear button
     } else if (event.target.textContent == "C") {
-        operationRequest = null;
+        input = null;
         firstNum = null;
-        secondNum = null;
         operator = null;
         screen.textContent = "0";
+    
+    //If press a number
+    } else if (!isNaN(event.target.textContent)){
+        //if starting from clear
+        if (input == null || input == 0) {
+            input = event.target.textContent
+            screen.textContent = input
+        //if extending a number (ie 1 becomes 12)    
+        } else {
+            input = input + event.target.textContent;
+            screen.textContent = input;
+        }
 
-    //If clear screen and is a number    
-    } else if ((operationRequest == null || operationRequest == 0)
-                && (!isNaN(event.target.textContent))) {
-        operationRequest = event.target.textContent;
-        screen.textContent = operationRequest;
-
-    //If screen is not clear and is a number    
-    } else if (!isNaN(event.target.textContent)) {
-        operationRequest = operationRequest + event.target.textContent;
-        calculatedTotal = null;
-        screen.textContent = operationRequest;
-
-    //If an operator and an operator hasn't been entered yet.
-    } else if (isNaN(event.target.textContent) && operator == null) {
-        operator =event.target.textContent;
-        firstNum = operationRequest;
-        operationRequest = null;
-        calculatedTotal = null;
-        screen.textContent = operator;
-
-    //If an operator and an operator HAS been entered.    
-    } else if (isNaN(event.target.textContent) && operator != null) {
-
+    //If input is an operator
+    } else if (isNaN(event.target.textContent)) {
+        //If first number is empty
+        if(firstNum == null) {
+            operator = event.target.textContent;
+            firstNum = input;
+            input = null;
+            screen.textContent = firstNum + operator;
+        } else if (firstNum != null) {
+            if (input == null) {
+                operator = event.target.textContent;
+            } else {
+                firstNum = operate(Number(firstNum), operator, Number(input));
+                operator = event.target.textContent;
+                input = null;
+                screen.textContent = firstNum + " " + operator;
+            }
+            
+        }
     }
 }
 
@@ -74,7 +88,12 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
-    return num1 / num2;
+    if (num2 == 0) {
+        return "I don't think so!";
+    }else {
+        return num1 / num2;
+    }
+    
 }
 
 function operate(num1, operator, num2){
